9cfc47e8eb7640a06948b7d9bdb08109d2ef9221
tuan.nguyenv <ext-tuan.nguyenv@spdigital.sg>
2020-10-14 15:27:00 +0700

    chore : first commit

---
+++ sixmin/sixminApp.swift
@@ -0,0 +1,17 @@
+//
+//  sixminApp.swift
+//  sixmin
+//
+//  Created by tuan.nguyenv on 10/14/20.
+//
+
+import SwiftUI
+
+@main
+struct sixminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
